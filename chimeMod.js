264a3261c65d2706884a86acbd7f4459c1c39dbe
omaruvu <114625539+omaruvu@users.noreply.github.com>
2025-04-07 03:23:20 +0200

    Update chimeMod.js

--- chimeMod.js
+++ chimeMod.js
@@ -5,7 +5,7 @@ Game.Upgrades["Golden cookie sound selector"].choicesFunction = function () {
     choices[2] = { name: 'Fortune', icon: [27, 6] };
     choices[3] = { name: 'Cymbal', icon: [9, 10] };
     choices[4] = { name: 'Squeak', icon: [8, 10] };
-    choices[5] = { name: 'Nyan there\'s a wrath cookie', icon: [0, 10] };
+    choices[5] = { name: 'Nyan there\'s a wrath cookie', icon: [0, 0, "https://omaruvu.github.io/nyan-chime-mod-thing/nyan.png"] };
     for (var i = 0; i < choices.length; i++) { choices[i].name = loc(choices[i].name); }
 
     choices[Game.chimeType].selected = 1;
@@ -17,5 +17,5 @@ Game.playGoldenCookieChime = () => {
     else if (Game.chimeType == 2) PlaySound('snd/fortune.mp3');
     else if (Game.chimeType == 3) PlaySound('snd/cymbalRev.mp3');
     else if (Game.chimeType == 4) { Game.wrinklerSquishSound++; if (Game.wrinklerSquishSound > 4) { Game.wrinklerSquishSound -= 4; } PlaySound('snd/squeak' + (Game.wrinklerSquishSound) + '.mp3'); }
-    else if (Game.chimeType == 5) PlaySound('snd/nyan.mp3');
+    else if (Game.chimeType == 5) PlaySound('https://omaruvu.github.io/nyan-chime-mod-thing/nyan.mp3');
 }
